20b14b3151fd4464ba899cedae761b174ea67f37
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2020-08-28 15:48:31 +0530

    Find the Duplicate Number

---
+++ 287. Find the Duplicate Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            int index=Math.abs(nums[i]);
+            if(nums[index]<0)
+                return index;
+            else
+                nums[index]=-nums[index];
+        }
+        return -1;
+    }
+}
