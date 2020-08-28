1260fd36056bd0b6e457e851b01aa099a4a95f31
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2020-08-28 15:43:17 +0530

    Contains Duplicate

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        
+        if(n==0)
+            return false;
+        
+        for(int i=0;i<n-1;i++)
+        {
+                if(nums[i]==nums[i+1])
+                    return true;                
+        }
+            
+        return false;
+    }
+}
