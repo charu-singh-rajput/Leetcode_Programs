2258ef8867ab02c111dc85a9fad457ed68898e17
Charu Singh <55341559+charu-singh-rajput@users.noreply.github.com>
2020-08-28 15:50:02 +0530

    Roman to Integer

---
+++ 13. Roman to Integer.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> roman=new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int add=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(roman.get(s.charAt(i))<roman.get(s.charAt(i+1)))
+            {
+                add-=roman.get(s.charAt(i));
+            }
+            else
+            {
+                add+=roman.get(s.charAt(i));
+            }
+        }
+        add+=roman.get(s.charAt(s.length()-1));
+        return add;
+        
+    }
+}
